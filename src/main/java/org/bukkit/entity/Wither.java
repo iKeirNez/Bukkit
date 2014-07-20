705b4e2e66fdcfd799f3ac1256921dd1d193fee2
Keir Nellyer <keir@nellyer.co.uk>
2014-07-20 20:27:17 +0100

    Change shoot method to return the WitherSkull shot WitherHead enum now also contains the IDs of each head type, mainly used internally

--- src/main/java/org/bukkit/entity/Wither.java
+++ src/main/java/org/bukkit/entity/Wither.java
@@ -28,16 +28,18 @@ public interface Wither extends Monster {
      *
      * @param head which head to fire from
      * @param entity the entity to fire at
+     * @return the wither skull that was shot
      */
-    public void shoot(WitherHead head, LivingEntity entity);
+    public WitherSkull shoot(WitherHead head, LivingEntity entity);
 
     /**
      * Fires a wither skull from a wither head.
      *
      * @param head which head to fire from
      * @param location the location to fire at
+     * @return the wither skull that was shot
      */
-    public void shoot(WitherHead head, Location location);
+    public WitherSkull shoot(WitherHead head, Location location);
 
     /**
      * An enum to specify a head on the Wither.
@@ -47,16 +49,51 @@ public interface Wither extends Monster {
         /**
          * The Left head in respect to a frontal view.
          */
-        LEFT,
+        LEFT(1),
 
         /**
          * The Center head in respect to a frontal view.
          */
-        CENTER,
+        CENTER(0),
 
         /**
          * The Right head in respect to a frontal view.
          */
-        RIGHT
+        RIGHT(2);
+
+        private int id;
+
+        private WitherHead(int id){
+            this.id = id;
+        }
+
+        /**
+         * Gets the head id of this WitherHead.
+         *
+         * @return ID of this WitherHead
+         * @deprecated Magic value
+         */
+        @Deprecated
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * Attempts to get a wither head by it's given ID.
+         *
+         * @param id ID of the wither head to get
+         * @return Wither head if found, else null
+         * @deprecated Magic value
+         */
+        @Deprecated
+        public static WitherHead getWitherHead(int id){
+            for (WitherHead witherHead : values()){
+                if (witherHead.getId() == id){
+                    return witherHead;
+                }
+            }
+
+            return null;
+        }
     }
 }
